3155fa62c6fe4bf823d0168df001b5ca50580e51
Elliott Polite <elliottp@iMacs-iMac.local>
2022-06-03 22:49:46 -0600

    completed exercise

---
+++ VariablesExercise/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace VariablesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dogName = "Chase";
+            int dogAge = 10;
+            char behaviorGrade = 'A';
+            bool barks = true;
+            double dinnerCost = 35.43;
+            decimal tipAmount = 12.5M;
+
+            Console.WriteLine($"I have a dog by the name of {dogName}. He is {dogAge} years old. {dogName} has a behavior grade of {behaviorGrade}");
+            Console.WriteLine("Does my dog bark? : {0}", barks);
+            Console.WriteLine($"Total cost of dinner was ${dinnerCost}, plus the ${tipAmount}");
+        }
+    }
+}
+
